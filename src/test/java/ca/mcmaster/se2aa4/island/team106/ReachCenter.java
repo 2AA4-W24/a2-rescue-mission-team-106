627e9e8ff35d67788a6bc06fd913ccc354e06ab8
vanessa-laii <vanessa.sw.lai@gmail.com>
2024-03-22 20:15:40 -0400

    reporter tests

--- src/test/java/ca/mcmaster/se2aa4/island/team106/ReachCenter.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team106/ReachCenter.java
@@ -1,4 +1,10 @@
 package ca.mcmaster.se2aa4.island.team106;
+import org.junit.jupiter.api.Test;
+import org.json.JSONObject;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 
 public class ReachCenter {
     
--- src/test/java/ca/mcmaster/se2aa4/island/team106/ReporterTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team106/ReporterTest.java
@@ -1,5 +1,48 @@
 package ca.mcmaster.se2aa4.island.team106;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ReporterTest {
-    
+    private MapArea mapArea;
+    private Reporter reporter;
+    private Set<Creek> creeks;
+
+    @BeforeEach
+    public void setUp() {
+        mapArea = new MapArea();
+        reporter = new Reporter(mapArea);
+        creeks = mapArea.getCreeks();
+
+    }
+
+    @Test
+    public void NoCreeks() {
+        creeks = mapArea.getCreeks();
+        String result = reporter.deliverReport();
+        assertEquals("No creek found", result);
+    }
+
+    @Test
+    public void EmergencySiteNotFound() {
+        Creek creek1 = new Creek(new Point(10, 20), "Creek1");
+        creeks.add(creek1);;
+        String result = reporter.deliverReport();
+        assertEquals("Creek1", result); 
+    }
+
+    @Test
+    public void EmergencySiteFound() {
+        Creek creek1 = new Creek(new Point(10, 20), "Creek1");
+        Creek emergency = new Creek(new Point(15, 25), "emergency");
+        creeks.add(creek1);
+        mapArea.setEmergencySite(emergency);
+        String result = reporter.deliverReport();
+        assertEquals("Creek1", result); 
+    }
 }
