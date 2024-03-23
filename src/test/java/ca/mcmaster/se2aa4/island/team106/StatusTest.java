547ca6aa4ee6d35ebba2d83921491614188ec2aa
vanessa-laii <vanessa.sw.lai@gmail.com>
2024-03-23 11:12:47 -0400

    add more Status Tests

--- src/test/java/ca/mcmaster/se2aa4/island/team106/StatusTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team106/StatusTest.java
@@ -8,7 +8,7 @@ import ca.mcmaster.se2aa4.island.team106.DroneTools.Status;
 import ca.mcmaster.se2aa4.island.team106.Drones.Drone;
 import ca.mcmaster.se2aa4.island.team106.Exploration.MapArea;
 
-public class DecisionMakerTest {
+public class StatusTest {
     private MapArea mapArea;
     private Drone drone;
 
@@ -19,15 +19,36 @@ public class DecisionMakerTest {
 
     }
     @Test
-    public void testMakeDecisionsGroundFinderState(){
+    public void testGroundFinderStatus(){
         drone.setStatus(Status.GROUND_FINDER_STATE);
         assertEquals(Status.GROUND_FINDER_STATE, drone.getStatus());
     }
 
     @Test
-    public void testMakeDecisionsCenterStartState(){
+    public void testCenterStartStatus(){
         drone.setStatus(Status.CENTER_START_STATE);
         assertEquals(Status.CENTER_START_STATE, drone.getStatus());
     }
+
+    @Test
+    public void testCenterStatus(){
+        drone.setStatus(Status.CENTER_STATE);
+        assertEquals(Status.CENTER_STATE, drone.getStatus());
+    }
+
+    @Test
+    public void testLengthStatus(){
+        drone.setStatus(Status.LENGTH_STATE);
+        assertEquals(Status.LENGTH_STATE, drone.getStatus());
+    }
+
+    @Test
+    public void testWidthStatus(){
+        drone.setStatus(Status.WIDTH_STATE);
+        assertEquals(Status.WIDTH_STATE, drone.getStatus());
+    }
+
+
+
 
 }
