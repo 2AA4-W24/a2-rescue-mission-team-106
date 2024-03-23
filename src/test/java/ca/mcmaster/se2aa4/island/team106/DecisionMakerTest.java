f2f48aeafffa1dda982b410ccadcaedde23830c4
vanessa-laii <vanessa.sw.lai@gmail.com>
2024-03-22 21:35:39 -0400

    cleaned up Decision Maker Test

--- src/test/java/ca/mcmaster/se2aa4/island/team106/DecisionMakerTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team106/DecisionMakerTest.java
@@ -15,27 +15,21 @@ public class DecisionMakerTest {
     private MapArea mapArea;
     private Drone drone;
     private FatalErrorHandler fatalErrorHandler;
-    private DecisionMaker decisionMaker;
-    private JSONObject parameters;
-    private JSONObject decision;
 
     @BeforeEach
     public void initialize(){
-        mapArea = new MapArea(); // Create a new instance of MapArea
+        mapArea = new MapArea(); 
         drone = new Drone(1000, mapArea); 
-        fatalErrorHandler = new FatalErrorHandler(50, drone, mapArea); // Create a new instance of FatalErrorHandler
-        decisionMaker = new DecisionMaker(drone, mapArea, fatalErrorHandler); // Create a new instance of DecisionMaker
-        parameters = new JSONObject();
-        decision = new JSONObject();
+
     }
     @Test
-    public void testMakeDecisionsGroundFinderState() {
+    public void testMakeDecisionsGroundFinderState(){
         drone.setStatus(Status.GROUND_FINDER_STATE);
         assertEquals(Status.GROUND_FINDER_STATE, drone.getStatus());
     }
 
     @Test
-    public void testMakeDecisionsCenterStartState() {
+    public void testMakeDecisionsCenterStartState(){
         drone.setStatus(Status.CENTER_START_STATE);
         assertEquals(Status.CENTER_START_STATE, drone.getStatus());
     }
