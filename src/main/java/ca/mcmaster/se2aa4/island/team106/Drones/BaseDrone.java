210a9f24f6882800f0810c01c5a204d5665496ab
Aditya <aditya.tripuraneni905@gmail.com>
2024-03-24 14:18:25 -0400

    Clean Drones Folder

--- src/main/java/ca/mcmaster/se2aa4/island/team106/Drones/BaseDrone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team106/Drones/BaseDrone.java
@@ -7,6 +7,7 @@ import ca.mcmaster.se2aa4.island.team106.DroneTools.Direction;
 import ca.mcmaster.se2aa4.island.team106.DroneTools.Status;
 import ca.mcmaster.se2aa4.island.team106.Exploration.MapArea;
 
+
 public abstract class BaseDrone{
     protected int minimumBatteryToOperate;
     protected int currentBatteryLevel;
@@ -15,103 +16,114 @@ public abstract class BaseDrone{
     protected MapArea mapArea;
 
 
-    /**
+    /*******************************************************************************
      * Constructs a new BaseDrone with the specified minimum battery level
      * required to operate and the map area it operates on.
      *
      * @param minimumBatteryToOperate the minimum battery level required to operate
      * @param mapArea the map area the drone operates on
-     */
+     *******************************************************************************/
     public BaseDrone(int minimumBatteryToOperate,  MapArea mapArea) {
         this.mapArea = mapArea;
         this.minimumBatteryToOperate = minimumBatteryToOperate; 
         this.status = Status.GROUND_FINDER_STATE;
     }
+
     
-    /**
+    /***********************************************************************************
      * Updates the drone based on the current battery level and the provided direction.
      *
      * @param batteryLevel the current battery level
      * @param direction the current direction of the drone
-     */
+     ************************************************************************************/
     public abstract void updateDrone(int batteryLevel, Direction direction);
 
-    /**
+
+    /**********************************
      * Gets the status of the drone.
      *
      * @return the status of the drone
-     */
+     **********************************/
     public abstract Status getStatus();
 
-    /**
+
+    /********************************************
      * Sets the status of the drone.
      *
      * @param status the new status of the drone
-     */
+     ********************************************/
     public abstract void setStatus(Status status);
 
-    /**
+
+    /*************************************************
      * Gets the current battery level of the drone.
      *
      * @return the current battery level of the drone
-     */
+     *************************************************/
     public abstract int getBatteryLevel();
+
     
-    /**
+    /*************************************************************************
      * Carries out the stop action for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
-     */
+     *************************************************************************/
     public abstract void stop(JSONObject parameter);
 
-    /**
+
+    /************************************************************
      * Carries out the fly action for the drone.
      *
      * @param decision  the decision JSON object to be modified
-     */
+     ************************************************************/
     public abstract void fly(JSONObject decision);
 
-    /**
+
+    /***************************************************************************
      * Checks if the drone can make a decision based on the given battery usage
      * of an action.
      *
      * @param batteryUsage the amount of battery used by the decision
      * @return true if the drone can make the decision, otherwise false
-     */
+     ***************************************************************************/
     public abstract boolean canMakeDecision(int batteryUsage);
 
-    /**
+
+    /****************************************************************
      * Update battery level of drone after performing the action
      *
      * @param batteryUsage the amount of battery used by the action
-     */
+     ****************************************************************/
     public abstract void useBattery(int batteryUsage); 
 
-    /**
+
+    /*************************************************************************
      * Carries out the echo action for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
      * @param decision  the decision JSON object to be modified
      * @param direction the direction for the echo action
-     */
+     *************************************************************************/
     public abstract void echo(JSONObject parameter, JSONObject decision, Direction direction);
 
-    /**
+
+    /************************************************************************
      * Carries out the heading action for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
      * @param decision  the decision JSON object to be modified
      * @param direction the new direction the drone should face
-     */
+     ************************************************************************/
     public abstract void updateHeading(JSONObject parameter, JSONObject decision, Direction direction);
 
-    /**
+
+    /***********************************************************
      * Carries out the scan action for the drone.
      *
      * @param decision  the decision JSON object to be modified
-     */
+     ***********************************************************/
     public abstract void scan(JSONObject decision); 
 }
--- src/main/java/ca/mcmaster/se2aa4/island/team106/Drones/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team106/Drones/Drone.java
@@ -6,67 +6,72 @@ import ca.mcmaster.se2aa4.island.team106.DroneTools.Direction;
 import ca.mcmaster.se2aa4.island.team106.DroneTools.Status;
 import ca.mcmaster.se2aa4.island.team106.Exploration.MapArea;
 
+
 public class Drone extends BaseDrone {
 
-    /**
+    /*****************************************************************************************
      * Constructs a Drone with the specified minimum battery level required to
      * operate and map area.
      *
-     * @param minimumBatteryToOperate the minimum battery level required to
-     * operate
+     * @param minimumBatteryToOperate the minimum battery level required to operate the drone
      * @param mapArea the map area on which the drone operates on
-     */
+     *****************************************************************************************/
     public Drone(int minimumBatteryToOperate, MapArea mapArea){
         super(minimumBatteryToOperate, mapArea);
     }
 
-    /**
+
+    /***********************************************************************************
      * Updates the drone based on the current battery level and the provided direction.
      *
      * @param batteryLevel the current battery level
      * @param direction the current direction of the drone
-     */
+     ************************************************************************************/
     @Override
     public void updateDrone(int batteryLevel, Direction direction){
         this.currentBatteryLevel = batteryLevel; 
         this.mapArea.setHeading(direction);
     }
 
-    /**
+
+    /*************************************************
      * Gets the current battery level of the drone.
      *
      * @return the current battery level of the drone
-     */
+     *************************************************/
     @Override
     public int getBatteryLevel(){
         return this.currentBatteryLevel; 
     }
 
-    /**
+
+    /**********************************
      * Gets the status of the drone.
      *
      * @return the status of the drone
-     */
+     **********************************/
     @Override
     public Status getStatus(){
         return this.status; 
     }
 
-    /**
+
+    /********************************************
      * Sets the status of the drone.
      *
      * @param status the new status of the drone
-     */
+     ********************************************/
     @Override
     public void setStatus(Status status){
         this.status = status; 
     }
 
-    /**
+
+    /************************************************************
      * Carries out the fly action for the drone.
      *
      * @param decision  the decision JSON object to be modified
-     */
+     ************************************************************/
     @Override
     public void fly(JSONObject decision){
         action.fly(decision);
@@ -75,14 +80,15 @@ public class Drone extends BaseDrone {
         this.mapArea.setCurrentAction("fly");
     }
 
-    /**
+
+    /**************************************************************************
      * Carries out the echo action for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
      * @param decision  the decision JSON object to be modified
      * @param direction the direction for the echo action
-     */
+     **************************************************************************/
     @Override
     public void echo(JSONObject parameter, JSONObject decision, Direction direction){
         switch (direction) {
@@ -107,37 +113,40 @@ public class Drone extends BaseDrone {
         this.mapArea.setCurrentAction("echo");
     }
 
-    /**
+
+    /*************************************************************
      * Carries out the stop action for the drone.
      *
-     * @param parameter the parameter JSON object that stores the additional
-     * parameters for the action
-     */
+     * @param parameter the parameter JSON object that stores the
+     * additional parameters for the action
+     *************************************************************/
     @Override
     public void stop(JSONObject decision){
         this.action.stop(decision);
         this.mapArea.setCurrentAction("stop");
     }
 
-    /**
+
+    /************************************************************
      * Carries out the scan action for the drone.
      *
      * @param decision  the decision JSON object to be modified
-     */
+     ************************************************************/
     @Override
     public void scan(JSONObject decision){
         this.action.scan(decision);
         this.mapArea.setCurrentAction("scan");
     }
 
-    /**
+
+    /*************************************************************
      * Carries out the heading action for the drone.
      *
-     * @param parameter the parameter JSON object that stores the additional
-     * parameters for the action
+     * @param parameter the parameter JSON object that stores the 
+     * additional parameters for the action
      * @param decision  the decision JSON object to be modified
      * @param direction the new direction the drone should face
-     */
+     *************************************************************/
     @Override
     public void updateHeading(JSONObject parameter, JSONObject decision, Direction updatedHeading){
         if (updatedHeading != this.mapArea.getHeading()){
@@ -148,35 +157,38 @@ public class Drone extends BaseDrone {
         }
     }
 
-    /**
-     * Checks if the drone can make a decision based on the given battery usage
-     * of an action.
+
+    /*******************************************************************
+     * Checks if the drone can make a decision based on the given
+     * battery usage of an action.
      *
      * @param batteryUsage the amount of battery used by the decision
      * @return true if the drone can make the decision, otherwise false
-     */
+     *******************************************************************/
     @Override
-    public boolean canMakeDecision(int batteryUsage) {
+    public boolean canMakeDecision(int batteryUsage){
         return (this.currentBatteryLevel - batteryUsage) >= this.minimumBatteryToOperate;
     }
 
-    /**
+
+    /***************************************************************
      * Update battery level of drone after performing the action
      *
      * @param batteryUsage the amount of battery used by the action
-     */
+     ***************************************************************/
     @Override
-    public void useBattery(int batteryUsage) {
+    public void useBattery(int batteryUsage){
         this.currentBatteryLevel -= batteryUsage;
     }
 
-    /**
+
+    /*******************************************************************
      * Carries out the echo action in the East direction for the drone.
      *
-     * @param parameter the parameter JSON object that stores the additional
-     * parameters for the action
-     * @param decision  the decision JSON object to be modified
-     */
+     * @param parameter the parameter JSON object that stores the
+     *                 additional parameters for the action
+     * @param decision the decision JSON object to be modified
+     *******************************************************************/
     private void echoEast(JSONObject parameter, JSONObject decision){
         if (this.mapArea.getHeading() != Direction.W){
             this.action.echo(parameter, decision, Direction.E);
@@ -188,13 +200,14 @@ public class Drone extends BaseDrone {
         }
     }
 
-    /**
+
+    /************************************************************************
      * Carries out the echo action in the West direction for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
      * @param decision  the decision JSON object to be modified
-     */
+     ************************************************************************/
     private void echoWest(JSONObject parameter, JSONObject decision){
         if (this.mapArea.getHeading() != Direction.E){
             this.action.echo(parameter, decision, Direction.W);
@@ -206,13 +219,14 @@ public class Drone extends BaseDrone {
         }
     }
 
-    /**
+
+    /************************************************************************
      * Carries out the echo action in the North direction for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
      * @param decision  the decision JSON object to be modified
-     */
+     ************************************************************************/
     private void echoNorth(JSONObject parameter, JSONObject decision){
         if (this.mapArea.getHeading() != Direction.S){
             this.action.echo(parameter, decision, Direction.N);
@@ -224,13 +238,14 @@ public class Drone extends BaseDrone {
         }
     }
 
-    /**
+
+    /************************************************************************
      * Carries out the echo action in the South direction for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
      * @param decision  the decision JSON object to be modified
-     */
+     ************************************************************************/
     private void echoSouth(JSONObject parameter, JSONObject decision){
         if (this.mapArea.getHeading() != Direction.N){
             this.action.echo(parameter, decision, Direction.S);
@@ -242,13 +257,14 @@ public class Drone extends BaseDrone {
         }
     }
 
-    /**
+
+    /************************************************************************
      * Carries out the echo action in the Forwards direction for the drone.
      *
      * @param parameter the parameter JSON object that stores the additional
      * parameters for the action
      * @param decision  the decision JSON object to be modified
-     */
+     ************************************************************************/
     private void echoForwards(JSONObject parameter, JSONObject decision){
         Direction currentHeading = mapArea.getHeading();
         this.action.echo(parameter, decision, currentHeading);
